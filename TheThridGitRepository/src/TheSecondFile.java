729ad7bfa53add4cf17f172ac081045a5b62580f
user name <user@email.com>
2014-05-27 13:51:10 -0400

    adding a for loop to send message.

--- TheThridGitRepository/src/TheSecondFile.java
+++ TheThridGitRepository/src/TheSecondFile.java
@@ -4,8 +4,9 @@ public class TheSecondFile {
 	public void sendMessage() {
 		String message[] = {"Git", "message"};
 		System.out.println("the second file");
+		System.out.println("sending message");
 		for (int i=0;i<message.length;i++) {
-			System.out.println("");
+			System.out.println("message: " + message[i]);
 		}
 	}
 }
