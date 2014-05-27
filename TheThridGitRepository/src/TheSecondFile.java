dfcf1611426ff831bdfd6abda2c9a8ab028fb748
user name <user@email.com>
2014-05-27 14:26:00 -0400

    Edited on moreBranch,sys out index value and added comments.

--- TheThridGitRepository/src/TheSecondFile.java
+++ TheThridGitRepository/src/TheSecondFile.java
@@ -1,12 +1,14 @@
 
 public class TheSecondFile {
-	
+	// the mail program. 
 	public void sendMessage() {
 		String message[] = {"Git", "message"};
 		System.out.println("the second file");
 		System.out.println("sending message");
 		for (int i=0;i<message.length;i++) {
+			System.out.println("print index i: " + i);
 			System.out.println("message: " + message[i]);
+			
 		}
 	}
 }
