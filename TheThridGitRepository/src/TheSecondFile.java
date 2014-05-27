8efa149310c2daeefa4ac40da912f7e45bf468ea
Administrator <Administrator@emit-test1>
2014-05-27 14:21:25 -0400

    Edited on changeDefault branch on TheSecondFile.java

--- TheThridGitRepository/src/TheSecondFile.java
+++ TheThridGitRepository/src/TheSecondFile.java
@@ -2,11 +2,14 @@
 public class TheSecondFile {
 	
 	public void sendMessage() {
-		String message[] = {"Git", "message"};
+		String message[] = {"Git", "message", "and","more", "message"};
 		System.out.println("the second file");
 		System.out.println("sending message");
-		for (int i=0;i<message.length;i++) {
-			System.out.println("message: " + message[i]);
+		for (String data: message) {
+			System.out.println("message: " + data);
 		}
+//		for (int i=0;i<message.length;i++) {
+//			System.out.println("message: " + message[i]);
+//		}
 	}
 }
