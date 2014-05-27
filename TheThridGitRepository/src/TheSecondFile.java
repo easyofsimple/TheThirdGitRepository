32fb8a4c097df139fd482e347416e1ab3fbd9546
user name <user@email.com>
2014-05-27 13:42:00 -0400

    partial implementation.

--- TheThridGitRepository/src/TheSecondFile.java
+++ TheThridGitRepository/src/TheSecondFile.java
@@ -1,6 +1,11 @@
 
 public class TheSecondFile {
+	
 	public void sendMessage() {
+		String message[] = {"Git", "message"};
 		System.out.println("the second file");
+		for (int i=0;i<message.length;i++) {
+			System.out.println("");
+		}
 	}
 }
