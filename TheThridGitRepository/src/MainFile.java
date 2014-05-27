54d3fd79d4f959401581202e9a8b3e1b38023262
user name <user@email.com>
2014-05-27 13:24:57 -0400

    Initial check in of TheThirdGitRepository

---
+++ TheThridGitRepository/src/MainFile.java
@@ -0,0 +1,8 @@
+
+public class MainFile {
+	public static void main(String [] args) {
+		TheSecondFile theSecondFile = new TheSecondFile();
+		theSecondFile.sendMessage();
+		System.out.println("the main file");
+	}
+}
---
+++ TheThridGitRepository/src/TheSecondFile.java
@@ -0,0 +1,6 @@
+
+public class TheSecondFile {
+	public void sendMessage() {
+		System.out.println("the second file");
+	}
+}
